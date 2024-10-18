b08dcd99f34a954662701fd8b0331296c554251e
fengh11 <fengh11@chinatelecom.cn>
2024-10-18 15:09:33 +0800

    fifth

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,16 +27,22 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
 
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
-mod Foo {
+mod foo {
     // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
+    #[no_mangle]
+    pub extern "Rust" fn my_demo_function(a: u32) -> u32 {
+        a
+    }
+
+    #[no_mangle]
+    #[link_name = "my_demo_function"]
+    pub extern "Rust" fn my_demo_function_alias(a: u32) -> u32 {
         a
     }
 }
